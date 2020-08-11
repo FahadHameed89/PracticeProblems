de5810bf5a856bd58da30b7cf354fcdfe600aee7
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-11 13:07:31 -0800

    Temp solution to problem

--- PracticeProblem11/Program.cs
+++ PracticeProblem11/Program.cs
@@ -6,7 +6,23 @@ namespace PracticeProblem11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello there, I will perform a function of your choice on two separate numbers of your choosing! Please enter the first operand");
+
+Console.WriteLine("Please enter the first number");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the operator you want to use");
+            string c = Console.ReadLine() ;
+
+            Console.WriteLine("Enter the last number");
+            int b = int.Parse(Console.ReadLine());
+
+            string formula = "acb";
+            formula = formula.Replace("a", a.ToString()).Replace("b", b.ToString()).Replace("c", c.ToString());
+
+            var calc = new System.Data.DataTable();
+            Console.WriteLine(calc.Compute(formula, ""));
+
         }
     }
 }
