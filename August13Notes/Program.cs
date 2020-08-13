3ad84b61f2d27b42f1ba8d078a7d58aa84084822
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-13 11:37:26 -0800

    Demonstration of first solution

--- August13Notes/Program.cs
+++ August13Notes/Program.cs
@@ -16,9 +16,46 @@ namespace August13Notes
             Last Modified: August 13, 2020
             */
 
-            string userInput, userChar, output;     // Declare variables
+            string userInput, userChar, output="";     // Declare variables
 
             Console.WriteLine("This program removes letters from any given string. Please enter a string and a character, and i'll remove the characters you enter from the string and give you a filtered message!");
+
+            Console.WriteLine("Please enter a string!");
+            userInput = Console.ReadLine();
+    
+            Console.WriteLine("Please enter a string to be filtered out!");
+            userChar = Console.ReadLine();
+
+
+            /*
+
+                or each character in the string:
+                    If that character is not the bad char
+                        Add that char to a new string
+
+            */
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] != userChar[0])
+                {
+                    output += userInput[i];
+                }
+            } 
+
+/* 
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (userInput[i] != userChar[0])
+                {
+                    userInput = userInput.Remove(i, 1);
+                    
+                }
+            }  */
+
+
+
+            Console.WriteLine($"Your filtered string is {output} !!");
 
 
         }
