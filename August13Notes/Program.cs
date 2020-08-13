88c19671b404e3e10e6c8bb83fc72485861a1872
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-13 14:53:43 -0800

    Copied JAmes' code

--- August13Notes/Program.cs
+++ August13Notes/Program.cs
@@ -6,16 +6,33 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            int age = -1, favourite = -1;
+            int[] myNumbers = new int[10];
 
-            age = GetValidInt("Please enter your age: ", 1, 100);
-     
-            favourite = GetValidInt("Please enter your favourite number: ", 0, 1000);
+            PopulateArray(myNumbers);
 
-            Console.WriteLine($"Your age is {age}, your favourite number is {favourite}.");
+            // foreach is useful for shorthand output but CANNOT edit array items, they will be marked as readonly.
+            foreach(int number in myNumbers)
+            {
+                Console.WriteLine(number);
+            }
 
-
+            /*
+            The above loop is exactly the same as this:
+            for (int i = 0; i < myNumbers.Length; i++)
+            {
+                Console.WriteLine(myNumbers[i]);
+            }
+            */
         }
+        // No return type due to the array being passed by reference (memory address) so any edits will affect the original.
+        static void PopulateArray(int[] theArray)
+        {
+            for(int i = 0; i < theArray.Length; i++)
+            {
+                theArray[i] = GetValidInt($"Please enter integer #{i+1}: ", 1, 100);
+            }
+        }
+
         static int GetValidInt(string prompt, int min, int max)
         {
             bool valid = false;
