918f5a81d05713f5faaa1b8b608533a41b1bca9e
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-13 11:10:05 -0800

    initial commit, added gitignore file

---
+++ August13Notes/Program.cs
@@ -0,0 +1,14 @@
+﻿// We are recreating the August 12th assignment with all challenges complete for the purpose of practice!!!
+
+using System;
+
+namespace August13Notes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
